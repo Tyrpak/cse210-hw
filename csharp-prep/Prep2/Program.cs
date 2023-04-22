c50869ba121269c0822c692af5c6ae9d0676af58
Tyrpak <greg@earthdawn.co.hu>
2023-04-22 21:47:11 +0200

    W01 Prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("How many points? ");
+        string userInput = Console.ReadLine();
+        int points = int.Parse(userInput);
+
+        string grade = "";
+
+        if (points >= 90)
+            {
+                grade = "A";
+            }
+        else if (points >= 80)
+            {
+                grade = "B";
+            }
+        else if (points >= 70)
+            {
+                grade = "C";
+            }
+        else if (points >= 60)
+            {
+                grade = "D";
+            }
+        else if (points < 60)
+            {
+                grade = "F";
+            }
+
+        Console.Write($"Your grade is: {grade}");
+
+        if (points >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
     }
 }
